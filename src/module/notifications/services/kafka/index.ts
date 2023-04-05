64faa79c56c90ce747ab4e5a6cc1b02cd90bb6ce
Enzo Israel <neyra.dianderas@gmail.com>
2023-04-04 22:36:37 -0500

    kafka service without registry

--- src/module/notifications/services/kafka/index.ts
+++ src/module/notifications/services/kafka/index.ts
@@ -1,70 +1,36 @@
 import { Kafka } from 'kafkajs';
 import type { Producer, Consumer, RecordMetadata } from 'kafkajs';
-import { SchemaRegistry } from '@kafkajs/confluent-schema-registry';
 
 export interface IKafkaService {
-  sendMessageToTopic({
-    key,
-    topic,
-    encodePayloadId,
-    payload,
-  }): Promise<RecordMetadata[]>;
+  sendMessageToTopic({ topic, value }): Promise<RecordMetadata[]>;
   subcribeMessageFromTopic(topic: string, callback: (value: any) => void);
-  findSchemaBySubjectAndVersion(
-    subject: string,
-    version: string | number
-  ): Promise<number>;
 }
 
 export class KafkaService implements IKafkaService {
   producer: Producer;
   consumer: Consumer;
-  registry: SchemaRegistry;
 
   constructor(setting: {
     brokers: string[];
-    host: string;
     clientId: string;
     groupId: string;
-    username?: string;
-    password?: string;
   }) {
-    const { username, password, brokers, host, clientId, groupId } = setting;
-    const sasl =
-      username && password ? { username, password, mechanism: 'plain' } : null;
-    const ssl = !!sasl;
+    const { brokers, clientId, groupId } = setting;
 
-    this.registry = new SchemaRegistry({ host });
-    const kafka = new Kafka({ clientId, brokers /*ssl sasl*/ });
+    const kafka = new Kafka({ clientId, brokers });
     this.producer = kafka.producer();
     this.consumer = kafka.consumer({ groupId });
   }
 
-  async findSchemaBySubjectAndVersion(
-    subject: string,
-    version: string | number
-  ): Promise<number> {
-    return await this.registry.getRegistryId(subject, version);
-  }
-
-  async sendMessageToTopic({
-    key,
-    topic,
-    encodePayloadId,
-    payload,
-  }): Promise<RecordMetadata[]> {
+  async sendMessageToTopic({ topic, value }): Promise<RecordMetadata[]> {
     try {
       await this.producer.connect();
-      const encodedPayload = await this.registry.encode(
-        encodePayloadId,
-        payload
-      );
 
       const responses = await this.producer.send({
         topic: topic,
-        messages: [{ key, value: encodedPayload }],
+        messages: [{ value }],
       });
-
+      await this.producer.disconnect();
       return responses;
     } catch (error) {
       console.error('Error trying to write data to Kafka', error);
@@ -81,12 +47,7 @@ export class KafkaService implements IKafkaService {
       await this.consumer.run({
         eachMessage: async ({ message }) => {
           try {
-            const decodedMessage = {
-              ...message,
-              // key: await registry.decode(message.key),
-              value: await this.registry.decode(message.value),
-            };
-            callback(decodedMessage);
+            callback(message);
           } catch (error) {
             console.log(error);
           }
